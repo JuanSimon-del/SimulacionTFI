89935c7e8d7305edb682421b833204e59ae97056
Luciana Ruiz <lucianaruiz.934@gmail.com>
2026-05-27 10:48:42 -0300

    Creé una barra lateral para mostrar la cantidad de campañas, cómo estás distribuidos los trabajadores y un sector en el que se puede elegir la información de que campaña se desea ver

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -4,18 +4,18 @@ import { Row, Col } from "react-bootstrap";
 
 function SideBar() {
   const trabajadores = [
-    { icono: "bi-person", nombre: "Inspección Visual" },
-    { icono: "bi-tools", nombre: "Desarme y Testeo" },
-    { icono: "bi-arrows-expand-vertical", nombre: "Separación de Materiales" },
-    { icono: "bi-recycle", nombre: "Reciclaje" },
+    { icono: "bi bi-eye", nombre: "Inspección Visual", color: "text-info" },
+    { icono: "bi-tools", nombre: "Desarme y Testeo", color: "text-secondary"},
+    { icono: "bi-arrows-expand-vertical", nombre: "Separación de Materiales", color: "text-warning" },
+    { icono: "bi-recycle", nombre: "Reciclaje", color: "text-success" },
   ];
 
   return (
     <>
-      <Navbar className="bg-danger w-25">
-        <section className="bg-info w-100 p-3">
+      <Navbar className="bg-dark w-25 vh-100 align-items-start">
+        <section className="w-100 p-3">
           <h3>Configuración Inicial</h3>
-          <article className="my-3">
+          <article className="my-4">
             <h5>Parámetros de la Simulación</h5>
             <Row className="d-flex my-3">
               <Col lg={8}>
@@ -26,33 +26,29 @@ function SideBar() {
               </Col>
             </Row>
           </article>
-          <article>
+          <article className="mb-4">
             <h5>Distribución de Trabajadores</h5>
             {trabajadores.map((trabajador, index) => (
               <Row key={index} className="d-flex align-items-center my-3">
                 <Col>
-                  <i className={`bi ${trabajador.icono} ms-3`}></i>
+                  <i className={`bi ${trabajador.icono} ${trabajador.color} ms-3`}></i>
                 </Col>
-                <Col lg={5}>
-                  <p className="mb-0">{trabajador.nombre}</p>
-                </Col>
-                <Col>
-                  <input type="text" className="mx-2 w-50" />
+                <Col lg={8}>
+                  <p className="mb-0 ms-2">{trabajador.nombre}</p>
                 </Col>
                 <Col>
-                  <p className="mb-0">trab.</p>
+                  <input type="text" className="me-2 w-100" />
                 </Col>
               </Row>
             ))}
           </article>
           <article>
-            <h5 className="my-3">Resultados por Campaña</h5>
-            <Button className="w-100 btn-secondary mb-1">Campaña 1</Button>
-            <Button className="w-100 btn-secondary mb-1">Campaña 2</Button>
-            <Button className="w-100 btn-secondary mb-1">Campaña 3</Button>
+            <h5 className="my-3">Resultados por Campaña (3)</h5>
+            <Button className="w-100 btn-secondary mb-2">Campaña 1</Button>
+            <Button className="w-100 btn-secondary mb-2">Campaña 2</Button>
+            <Button className="w-100 btn-secondary mb-2">Campaña 3</Button>
           </article>
         </section>
-        
       </Navbar>
     </>
   );
